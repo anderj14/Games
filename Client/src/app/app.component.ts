971eb1f9be22e08f5c118f967e4cffbd8118c55c
anderj14 <andersonfrias001@gmail.com>
2024-01-29 12:41:26 -0400

    change of angular version to 15

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,20 +1,9 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { NavBarComponent } from './core/nav-bar/nav-bar.component';
-import { FooterComponent } from './core/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterOutlet, NavBarComponent, FooterComponent],
-  template: `
-    <!-- <p>hola</p> -->
-    <app-nav-bar />
-    <router-outlet></router-outlet>
-    <app-footer></app-footer>
-  `,
-  styles: ''
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'Client';
